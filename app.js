83ac591af7133c8ccdeadc09d2c763a24c15d8b5
mokamel2000 <116734245+mokamel2000@users.noreply.github.com>
2024-02-15 09:21:03 +0100

    Changed the display

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ let id_identificator = "VisitorWidgetStyleWrapper__WidgetStyleWrapper-sc-1nz0dzb
 let getChatBoxElement = document.getElementsByClassName(id_identificator);
 let body = document.body;
 
-
 const linkedInElement = document.createElement("div");
 const linkedInLogoBackground = document.createElement("div");
 const linkinLogo = document.createElement("img");
 const leftSection = document.createElement("div");
+
+
 
 
 
@@ -74,7 +75,6 @@ linkedInElement.style.borderTopRightRadius = "15px"
 linkedInElement.style.borderTopLeftRadius = "20px"
 linkedInElement.style.cursor = "pointer";
 
-
 function positionPara() {
     let chatboxRect = getChatBoxElement.getBoundingClientRect();
 
@@ -91,5 +91,33 @@ function positionPara() {
 positionPara();
 
 
+function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                observer.disconnect();
+                resolve(document.querySelector(selector));
+            }
+        });
+
+        // If you get "parameter 1 is not of type 'Node'" error, see https://stackoverflow.com/a/77855838/492336
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
+waitForElm('.some-class').then((elm) => {
+    linkedInElement.style.display = "inline";
+});
+
+
 window.addEventListener('resize', positionPara);
 linkedInElement.addEventListener("click", () => window.location.href = linkToLinkedInUser);
+
+
